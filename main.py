52d7aa7961b8fbb42871a16e439225b6aa72e974
Prayank Kulshrestha <50411253+Prayankkulshrestha@users.noreply.github.com>
2025-10-03 16:46:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 from workflow import run_graph
 import config
+from argparse import ArgumentParser
 
-config.set_date_range("2025-09-01", "2025-03-01")
-print(config.START_DATE)
-print(config.END_DATE)
+parser = ArgumentParser()
 
+parser.add_argument("--category",help="Category for want insights")
+parser.add_argument("--start_date",help="from date you want insight")
+parser.add_argument("--end_date",help="Till which date you want insights")
 
-state = {"category":"Energy Drinks"}
+args = parser.parse_args()
+
+start_date = args[1]
+end_date= args[2]
+category = args[0]
+
+config.set_date_range(end_date=end_date, start_date=start_date)
+
+state = {"category":category}
 results = run_graph(state=state)
 print("From cache?", results.get("_from_cache"))
 print("Final report:", results.get("final_report"))
